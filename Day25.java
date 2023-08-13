7fe93a3f181a4afc77ca43ed27aaed6bb66c3d5b
Muskan Pandey <muskanpandey1003@gmail.com>
2023-08-14 04:51:15 +0530

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,33 @@
+//Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    void solve(int[] nums, int[] per, int c) {
+        if (c == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : per) {
+                permutation.add(num);
+            }
+            ans.add(permutation);
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (per[i] == 11) {
+                per[i] = nums[c];
+                solve(nums, per, c + 1);
+                per[i] = 11;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+        int[] per = new int[n];
+        for (int i = 0; i < n; i++) {
+            per[i] = 11;
+        }
+        solve(nums, per, 0);
+        return ans;
+    }
+}
