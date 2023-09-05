bcc6560da68df374657e9828c462efb38d8ecfd5
Muskan Pandey <muskanpandey1003@gmail.com>
2023-09-05 23:47:03 +0530

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,14 @@
+//217. Contains Duplicate
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+}
