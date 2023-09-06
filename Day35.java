4525f8b9fc3a496153160f6289469b68e4b7eb86
Muskan Pandey <muskanpandey1003@gmail.com>
2023-09-06 22:51:40 +0530

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,22 @@
+//167. Two Sum II - Input Array Is Sorted
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n =numbers.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            if(numbers[left]+numbers[right]==target){
+                return new int[]{left+1, right+1};
+            }
+            else if(numbers[left]+numbers[right]<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }return new int[]{-1,-1};
+        
+    }
+}
