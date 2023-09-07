e0035f26b6efb7dc3488353cec31c60700a6fa4b
Muskan Pandey <muskanpandey1003@gmail.com>
2023-09-08 00:07:18 +0530

    Create Day40.java

---
+++ Day40.java
@@ -0,0 +1,12 @@
+//1299. Replace Elements with Greatest Element on Right Side
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int rightMax = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int newMax = Math.max(rightMax, arr[i]);
+            arr[i] = rightMax;
+            rightMax = newMax;
+        }
+        return arr;
+    }
+}
