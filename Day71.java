1eb38f8823f185de895033a6460797421504076c
Muskan Pandey <muskanpandey1003@gmail.com>
2023-09-22 19:57:11 +0530

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,24 @@
+//Contains Duplicate II
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        Set<Integer> window = new HashSet<>();
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+
+            if (window.size() > k) {
+                window.remove(nums[left]);
+                left++;
+            }
+
+            if (window.contains(nums[right])) {
+                return true;
+            }
+
+            window.add(nums[right]);
+        }
+
+        return false;
+    }
+}
