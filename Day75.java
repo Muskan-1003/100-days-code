b3e8bdbdd4d31e7843b7ff38e8876a34441d375a
Muskan Pandey <muskanpandey1003@gmail.com>
2023-09-24 00:00:23 +0530

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,18 @@
+//Length of Last Word
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        s = s.trim();
+        
+        int length = 0;
+        int i = s.length() - 1;
+        
+      
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
+    }
+}
